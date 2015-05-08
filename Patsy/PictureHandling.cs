212870ca8a233468195ca08f3a39cd1c7b5cef3f
fw <fireworx@gmail.com>
2015-05-08 18:51:12 +0200

    Initial commit

---
+++ Patsy/PictureHandling.cs
@@ -0,0 +1,38 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Threading;
+
+namespace Patsy
+{
+    class PictureHandling
+    {
+        private string _fileFormatString;
+
+        public void TakePicture(string fileFormatString)
+        {
+            _fileFormatString = fileFormatString;
+            // enumerate video devices
+            var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            // create video source
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            // set NewFrame event handler
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            // start the video source
+            videoSource.Start();
+
+            // signal to stop when you no longer need capturing
+            Thread.Sleep(500); // 500ms seems like a good enough value for just taking one picture with default settings.
+            videoSource.SignalToStop();
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // get new frame
+            Bitmap bitmap = eventArgs.Frame;
+            bitmap.Save(_fileFormatString);
+            // process the frame
+        }
+    }
+}
---
+++ Patsy/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using WMPLib;
+
+namespace Patsy
+{
+    class Program
+    {
+        private const int START_HOUR = 9;
+        private const int FINISH_HOUR = 17;
+
+        static void Main(string[] args)
+        {
+            WindowsMediaPlayer player = new WindowsMediaPlayer();
+            string[] files = Directory.GetFiles(@"C:\Users\fw\Music\patsy-ljud");
+
+            Random rand = new Random();
+            PictureHandling pictureHandling = new PictureHandling();
+            string fileFormat = string.Empty;
+
+            while (true)
+            {
+                if (DateTime.Now.Hour >= START_HOUR && DateTime.Now.Hour <= FINISH_HOUR)
+                {
+                    foreach (var file in files)
+                    {
+                        Console.Clear();
+
+                        var next = rand.Next(1000 * 30, 120 * 1000);
+                        Console.Write("Sleeping for: {0}s\n", (next / 1000));
+                        Thread.Sleep(next);
+
+                        Console.Write("Playing {0}\n", file);
+                        player.URL = file;
+                        player.controls.play();
+
+                        Thread.Sleep(1000 * 10); // wait for patsy to show up.
+                        fileFormat = string.Format("{0}{1}", DateTime.Now.ToString().Replace(":", "-"), ".bmp");
+                        pictureHandling.TakePicture(fileFormat);
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Not yet time");
+                }
+
+                Thread.Sleep(1000 * 10);
+            }
+        }
+
+
+    }
+}
