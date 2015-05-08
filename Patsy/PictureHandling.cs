58ee0e1a20184184d8a407380cffdb63c888e586
fw <fireworx@gmail.com>
2015-05-08 19:15:15 +0200

    Checking to see if no cameras was found. In that case, return.

--- Patsy/PictureHandling.cs
+++ Patsy/PictureHandling.cs
@@ -1,6 +1,5 @@
 ﻿using AForge.Video;
 using AForge.Video.DirectShow;
-using System;
 using System.Drawing;
 using System.Threading;
 
@@ -15,15 +14,21 @@ namespace Patsy
             _fileFormatString = fileFormatString;
             // enumerate video devices
             var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count < 1)
+                return; // no videoDevices was found.
+
             // create video source
             VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
             // set NewFrame event handler
             videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+
             // start the video source
             videoSource.Start();
 
+            // 500ms seems like a good enough value for just taking one picture with default settings.
+            Thread.Sleep(500);
+
             // signal to stop when you no longer need capturing
-            Thread.Sleep(500); // 500ms seems like a good enough value for just taking one picture with default settings.
             videoSource.SignalToStop();
         }
 
@@ -31,8 +36,8 @@ namespace Patsy
         {
             // get new frame
             Bitmap bitmap = eventArgs.Frame;
+            // save it
             bitmap.Save(_fileFormatString);
-            // process the frame
         }
     }
 }
